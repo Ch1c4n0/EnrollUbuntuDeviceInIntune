a1953bd9eda3d789cc5d190c8486f4e1a8aa48b3
Marcelo Gonçalves <122677916+Ch1c4n0@users.noreply.github.com>
2024-04-06 16:37:17 -0300

    Create EnrollUbuntu22.sh

---
+++ EnrollUbuntu22.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+sudo apt install microsoft-edge-dev
+sudo apt update
+sudo apt upgrade
+
+
+sudo apt install curl gpg
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/ 
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" > /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list' 
+sudo rm microsoft.gpg
+
+sudo apt update
+sudo apt install intune-portal
